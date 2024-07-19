70084e409ed0759b23cb53115aaf03542887bd92
Jay_Shanker <jayshankarprasad57@gmail.com>
2024-07-19 16:26:33 +0530

    initial commit

--- src/EX_19072024/Lab001.java
+++ src/EX_19072024/Lab001.java
@@ -1,7 +1,12 @@
 package EX_19072024;
+import java.lang.Math;
 
 public class Lab001 {
     public static void main(String[] args) {
+        System.out.println("Square root of 2 is " + Math.sqrt(2));
+        System.out.println("Cube root of 3 is " + Math.cbrt(3));
+        System.out.println("Addition of 2 and 3 is " + Math.addExact(2,3));
+
 
     }
 }
